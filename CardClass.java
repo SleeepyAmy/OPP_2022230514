60aada48d7f75e89895f600274a105b6394c2659
SleeepyAmy <136402735+SleeepyAmy@users.noreply.github.com>
2025-03-17 22:14:14 +0100

    CardClass.java

--- CardClass.java
+++ CardClass.java
@@ -1,4 +1,9 @@
-// Enum for memory card class type
+// hi , This is an enum for memory card speed classes
+// It has three fixed values: CLASS_4, CLASS_6, and CLASS_10
+// Each class represents a different speed category for a memory card
+
 public enum CardClass {
-    CLASS_4, CLASS_6, CLASS_10
+    CLASS_4,  // Slower speed, used for basic storage or something like this
+    CLASS_6,  // Medium speed, good for normal recording
+    CLASS_10  // Fast speed, best for HD recording and high performance and so
 }
